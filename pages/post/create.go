456566d9e46fa56de6ab17db196449b20152d0c6
Alexander Johnston <Aliics@hotmail.com>
2022-03-13 11:08:36 +1300

    post creation

--- pages/post/create.go
+++ pages/post/create.go
@@ -1,6 +1,8 @@
 package post
 
 import (
+	"errors"
+	"github.com/eyebrow-fish/stupid-simple-blog/db"
 	"github.com/eyebrow-fish/stupid-simple-blog/pages"
 	"net/http"
 )
@@ -12,6 +14,22 @@ type postForm struct {
 
 func Create(w http.ResponseWriter, r *http.Request) {
 	pages.FormHandler[postForm](w, r, postForm{}, func(p postForm) error {
+		if p.Title == "" {
+			return errors.New("title cannot be empty")
+		}
+		if p.Text == "" {
+			return errors.New("text cannot be empty")
+		}
+
+		r, err := db.DB.Query(`
+			insert into posts(title, text, user_id)
+			values($1, $2, 1)
+		`, p.Title, p.Text)
+		if err != nil {
+			return err
+		}
+		defer func() { _ = r.Close() }()
+
 		return nil
 	})
 }
